7924b094b2f70d11c38278db2b5077b33e409671
calm-fork-7535 <komald.4@gmail.com>
2020-01-22 17:00:41 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -32,7 +32,7 @@ def find_greater_pair(src)
   end
 
   result
-end  
+end
 
 
 def total_even_pairs(src)
@@ -43,4 +43,14 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  total = 0
+  i = 0
+  while i < src.length do
+    if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
+      total += (src[i][0] + src[i][1])
+    end
+    i += 1
+  end
+
+  total
 end
